073f989ba43e6dce0902fbae93c10e7de4742400
James Joy <joyja@joyautomation.com>
2024-09-05 07:05:54 -0700

    save the files I forgot to save

--- index.ts
+++ index.ts
@@ -1,2 +1,35 @@
-export { createNode, disconnectNode } from "./stateMachines/node.ts";
-export { createHost, disconnectHost } from "./stateMachines/host.ts";
+/**
+ * @module stateMachines
+ */
+
+/**
+ * Creates a new node.
+ * @function
+ * @name createNode
+ * @memberof module:stateMachines
+ */
+export { createNode } from "./stateMachines/node.ts";
+
+/**
+ * Disconnects a node.
+ * @function
+ * @name disconnectNode
+ * @memberof module:stateMachines
+ */
+export { disconnectNode } from "./stateMachines/node.ts";
+
+/**
+ * Creates a new host.
+ * @function
+ * @name createHost
+ * @memberof module:stateMachines
+ */
+export { createHost } from "./stateMachines/host.ts";
+
+/**
+ * Disconnects a host.
+ * @function
+ * @name disconnectHost
+ * @memberof module:stateMachines
+ */
+export { disconnectHost } from "./stateMachines/host.ts";
