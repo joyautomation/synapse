5b473053b08e211b80fa91e229e72ef6032f6340
James Joy <joyja@joyautomation.com>
2024-09-17 19:43:21 -0700

    automatic rebirth occurs when ddata, ndata or dbirth is recieved for a node that doesn't exist

--- stateMachines/node.test.ts
+++ stateMachines/node.test.ts
@@ -1,12 +1,24 @@
 import { describe, it } from "@std/testing/bdd";
 import { expect } from "@std/expect";
-import { createNode, disconnectNode } from "./node.ts";
-import type { SparkplugCreateNodeInput, SparkplugNode } from "../types.d.ts";
+import {
+  createNode,
+  disconnectNode,
+  metricNeedsToPublish,
+  nodeTransitions,
+} from "./node.ts";
+import type {
+  SparkplugCreateNodeInput,
+  SparkplugMetric,
+  SparkplugNode,
+} from "../types.d.ts";
 import type { IDisconnectPacket, MqttClient } from "mqtt";
 import type mqtt from "mqtt";
-import { assertSpyCalls, spy, stub } from "@std/testing/mock";
+import { assertSpyCalls, Spy, spy, stub } from "@std/testing/mock";
 import { EventEmitter } from "node:events";
 import { _internals } from "../mqtt.ts";
+import { Buffer } from "node:buffer";
+import { encodePayload } from "../mqtt.ts";
+import { getUnixTime } from "date-fns";
 
 const connectPacket: mqtt.IConnackPacket = {
   cmd: "connack",
@@ -17,6 +29,12 @@ const connectPacket: mqtt.IConnackPacket = {
 const disconnectPacket: IDisconnectPacket = {
   cmd: "disconnect",
 };
+
+stub(console, "debug");
+stub(console, "info");
+stub(console, "error");
+const warnStub = stub(console, "warn");
+// stub(console, "log");
 
 describe("Node", () => {
   const mockConfig: SparkplugCreateNodeInput = {
@@ -41,17 +59,13 @@ describe("Node", () => {
             name: "testMetric",
             type: "String",
             value: "testValue",
+            scanRate: 1000,
           },
         },
       },
     },
   };
   let node: SparkplugNode;
-  stub(console, "debug");
-  stub(console, "info");
-  stub(console, "error");
-  stub(console, "warn");
-  stub(console, "log");
   it("creates a node with correct properties", () => {
     const mockClient = new EventEmitter() as unknown as mqtt.MqttClient;
     mockClient.subscribe = spy() as typeof MqttClient.prototype.subscribe;
@@ -116,6 +130,56 @@ describe("Node", () => {
         dead: false,
       },
       disconnected: false,
+    });
+  });
+  describe("birth transition", () => {
+    it("publishes a birth message if the mqtt client exists.", () => {
+      const calls = (node.mqtt?.publish as Spy).calls.length;
+      nodeTransitions.birth(node);
+      expect(node.mqtt?.publish).toBeDefined();
+      if (node.mqtt?.publish) {
+        assertSpyCalls(node.mqtt.publish as Spy, calls + 1);
+      }
+    });
+    it("publishes nothing if the mqtt client doesn't exist.", () => {
+      nodeTransitions.birth({} as SparkplugNode);
+      assertSpyCalls(warnStub, 1);
+    });
+  });
+  describe("Report By Exception", () => {
+    it("is true if lastPublished value is null", () => {
+      const testMetric: SparkplugMetric = {
+        name: "testMetric",
+        type: "Int32",
+        value: 101,
+        deadband: {
+          value: 100,
+          maxTime: 1000,
+        },
+        lastPublished: {
+          timestamp: getUnixTime(new Date()),
+          value: null,
+        },
+        scanRate: 1000,
+      };
+      expect(metricNeedsToPublish(testMetric)).toBe(true);
+    });
+    it("is false if lastPublished value is not null", () => {
+      const testMetric: SparkplugMetric = {
+        name: "testMetric",
+        type: "Int32",
+        value: 101,
+        deadband: {
+          value: 100,
+          maxTime: 1000,
+        },
+        lastPublished: {
+          timestamp: getUnixTime(new Date()),
+          value: 100,
+        },
+        scanRate: 1000,
+      };
+      expect(metricNeedsToPublish(testMetric)).toBe(false);
     });
   });
   it("disconnects properly when we tell it to", async () => {
