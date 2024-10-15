4ca992a81f0615a597abdae8e3fef649b5d9e229
Adlai Arnold <adlai.arnold@primestagetechnology.com>
2024-10-15 14:12:51 -0700

    script to fix types import after build

--- scripts/build_npm.sh
+++ scripts/build_npm.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -e
+
 # Check if VERSION parameter is provided
 if [ $# -eq 0 ]; then
     echo "Error: VERSION parameter is required."
@@ -14,8 +16,13 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 # Run the build script
 deno run -A ${SCRIPT_DIR}/build_npm.ts "$VERSION"
+echo "Build completed."
 
 # Move types.d.ts file
 cp ${SCRIPT_DIR}/../npm/src/types.d.ts ${SCRIPT_DIR}/../npm/esm/types.d.ts
+echo "types.d.ts moved successfully."
 
-echo "Build completed and types.d.ts moved successfully."
+# Replace export type * from "./types" with export type * from "./types.d.ts" in index.d.ts
+sed -i '' 's|export type \* from "\./types"|export type \* from "\./types.d.ts"|' ${SCRIPT_DIR}/../npm/esm/index.d.ts
+echo "Updated index.d.ts to use correct types import."
+
