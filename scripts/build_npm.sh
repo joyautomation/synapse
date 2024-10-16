1b36b82ab3493eb700c96ad7cfe2ede706d311ce
James Joy <joyja@joyautomation.com>
2024-10-15 19:37:52 -0700

    Bump version to 0.0.42

--- scripts/build_npm.sh
+++ scripts/build_npm.sh
@@ -23,6 +23,6 @@ cp ${SCRIPT_DIR}/../npm/src/types.d.ts ${SCRIPT_DIR}/../npm/esm/types.d.ts
 echo "types.d.ts moved successfully."
 
 # Replace export type * from "./types" with export type * from "./types.d.ts" in index.d.ts
-sed -i '' 's|export type \* from "\./types"|export type \* from "\./types.d.ts"|' ${SCRIPT_DIR}/../npm/esm/index.d.ts
+sed -i 's|export type \* from "\./types"|export type \* from "\./types.d.ts"|' ${SCRIPT_DIR}/../npm/esm/index.d.ts
 echo "Updated index.d.ts to use correct types import."
 
